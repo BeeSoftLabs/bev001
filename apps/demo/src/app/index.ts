a46a0ef1b7e3a608ff9ded9aa42dc7cd12e79be6
Bram Borggreve <borggreve@gmail.com>
2020-12-12 17:16:04 -0500

    finalizando stream

--- apps/demo/src/app/index.ts
+++ apps/demo/src/app/index.ts
@@ -1,8 +1,57 @@
+import { PrismaClient } from '@prisma/client'
 import { log } from './utils'
 
 export async function index() {
-  log('Bienvenido a BeeSoftLabs!')
-  log('HOLA BEV001!')
+  const prisma = new PrismaClient()
+
+  // await prisma.user.create({
+  //   data: {
+  //     username: 'derlys',
+  //   },
+  // })
+
+  // await prisma.user.update({
+  //   where: { username: 'beeman' },
+  //   data: { username: 'bee' },
+  // })
+
+  // await prisma.user.delete({
+  //   where: { username: 'bee' },
+  // })
+
+  // const users = await prisma.user.findMany()
+  // log(users)
+  //
+  // const count = await prisma.user.count()
+  //
+  // log(count)
+
+  await prisma.post.create({
+    data: {
+      text: 'Hola Prisma',
+      author: { connect: { username: 'derlys' } },
+      comments: {
+        create: { author: { connect: { username: 'derlys' } }, text: 'Hola Comment' },
+      },
+    },
+  })
+
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+      comments: {
+        include: {
+          author: true,
+        },
+      },
+    },
+  })
+  log(posts)
+
+  const count = await prisma.post.count()
+  log(count)
+  // log('Bienvenido a BeeSoftLabs!')
+  // log('HOLA BEV001!')
   // log({
   //   0: 'BEV001 - Prisma Desde Cero',
   //   1: 'Qué es Prisma?',
--- apps/demo/src/app/utils.ts
+++ apps/demo/src/app/utils.ts
@@ -2,6 +2,6 @@ import colorize from 'json-colorizer'
 import { JsonValue } from 'type-fest'
 import { gray } from 'chalk'
 
-export function log(item: JsonValue) {
+export function log(item: any) {
   console.log(gray(`${typeof item}:`), colorize(JSON.stringify(item, null, 2)))
 }
